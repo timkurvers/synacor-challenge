6ee363ff7c4a2c22eedc524ec81bdc0f6dc4f3ae
Tim Kurvers <tim@moonsphere.net>
2019-08-31 14:00:47 +0200

    Add executable for running a program

---
+++ index.mjs
@@ -0,0 +1,12 @@
+import Program from './src/Program';
+import VM from './src/VM';
+
+const name = process.argv[2];
+if (!name) {
+  console.error('No program name specified');
+  process.exit(1);
+}
+
+const vm = new VM();
+const program = Program.for(name);
+vm.run(program);
