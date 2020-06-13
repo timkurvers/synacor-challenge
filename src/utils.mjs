2eaf2a2e626b1be7a969967521ac44153f63103f
Tim Kurvers <tim@moonsphere.net>
2020-06-14 01:19:09 +0200

    Add hex32 utility and support for little endian output

--- src/utils.mjs
+++ src/utils.mjs
@@ -2,17 +2,24 @@ import { ADDRESS_SIZE } from './constants';
 
 export { default as colors } from 'colors/safe';
 
-export const hex = (value, { bitSize = 0, prefix = false } = {}) => {
-  const result = value.toString(16).padStart(bitSize / 4, '0');
+export const hex = (value, { bitSize = 0, le = false, prefix = false } = {}) => {
+  let result = value.toString(16).padStart(bitSize / 4, '0');
+  if (le) {
+    result = result.split(/([a-f\d]{2})/).reverse().join('');
+  }
   return prefix ? `0x${result}` : result;
 };
 
-export const hex8 = (value, { prefix = false } = {}) => (
-  hex(value, { bitSize: 8, prefix })
+export const hex8 = (value, { le = false, prefix = false } = {}) => (
+  hex(value, { bitSize: 8, le, prefix })
 );
 
-export const hex16 = (value, { prefix = false } = {}) => (
-  hex(value, { bitSize: 16, prefix })
+export const hex16 = (value, { le = false, prefix = false } = {}) => (
+  hex(value, { bitSize: 16, le, prefix })
+);
+
+export const hex32 = (value, { le = false, prefix = false } = {}) => (
+  hex(value, { bitSize: 32, le, prefix })
 );
 
 export const hexoffset = (address, { bitSize = 16, prefix = true } = {}) => (
